0dcc338a23644c3fe6ce30cd00a135fb5640c23d
Nawaraj <shah.nawaraj.ns@gmail.com>
2022-12-31 18:12:36 +0545

    last edit before firebase

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,5 +1,5 @@
 // photo
-import votingphoto from '../../assets/Voting-in-College.webp'
+import votingPhoto from '../../assets/Voting-in-College.webp'
 
 // styles
 import styles from './Login.module.css'
@@ -7,27 +7,29 @@ import styles from './Login.module.css'
 export default function login() {
     return (
         <>
-            <img src={votingphoto} className={styles.mainphoto}/>
+            <img src={votingPhoto} className={styles.mainPhoto}/>
             <div className={styles.loginBackground}>
                 <h1>Sign In</h1>
                 <h5>Sign in and Vote for your Candidates</h5>
                 <form>
-                    {/*
-                    <label>Enter your college email </label>
-                    */}
-                    <input 
-                        type="text" 
-                        placeholder="Enter your College Email"
-                    ></input>
+                    <div>
+                        <label>Email address:  
+                            <input 
+                                type="text" 
+                                placeholder="Enter your College Email"
+                            ></input>
+                        </label>
+                    </div>
 
-                    {/*
-                    <label>Enter your password </label>
-                    */}
                     <br/>
-                    <input 
-                        type="password" 
-                        placeholder="Enter your password"
-                    ></input>
+                    <div>
+                        <label>Password:
+                            <input 
+                                type="password" 
+                                placeholder="Enter your password"
+                            ></input>
+                        </label>
+                    </div>
                 </form>
                 <button className={styles.loginButton}>Login</button>
             </div>
